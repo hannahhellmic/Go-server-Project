45d582cf36f0939da1b8d27298dc6ddb79748abf
hannahhellmic <167358202+hannahhellmic@users.noreply.github.com>
2024-07-09 15:59:18 +0300

    Create main.go

---
+++ ServerProject/cmd/server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"awesomeProject/accounts"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	accountsHandler := accounts.New()
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/account", accountsHandler.GetAccount)
+	e.POST("/account/create", accountsHandler.CreateAccount)
+	e.DELETE("/account", accountsHandler.DeleteAccount)
+	e.PATCH("/account/changebalance", accountsHandler.PathAccount)
+	e.PATCH("/account/change", accountsHandler.ChangeAccount)
+	e.PATCH("/account/transfer", accountsHandler.TransferAccount)
+	e.GET("account/all", accountsHandler.ListAccounts)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
